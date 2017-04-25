85a944f5a4c9d5d5322954d9a328174b564d75fc
Crispin Wellington <retrogradeorbit@gmail.com>
2017-04-25 11:05:43 +0800

    code cleanup

--- src/thekeep/controls.cljs
+++ src/thekeep/controls.cljs
@@ -10,18 +10,15 @@
    (gp/button-pressed? 0)
    (gp/button-pressed? 1)
    (gp/button-pressed? 2)
-   (gp/button-pressed? 3)
-   )
-  )
+   (gp/button-pressed? 3)))
 
 (defn direction []
   (vec2/vec2
    (or (gp/axis 0)
        (cond (e/is-pressed? :left) -1
              (e/is-pressed? :right) 1
-             :default 0) )
+             :default 0))
    (or (gp/axis 1)
        (cond (e/is-pressed? :up) -1
              (e/is-pressed? :down) 1
-             :default 0)
-       )))
+             :default 0))))
