5dc5132d6618050a8b8956427cbc0ef4fddd50db
Crispin Wellington <retrogradeorbit@gmail.com>
2017-04-25 11:08:18 +0800

    cleanup

--- src/thekeep/state.cljs
+++ src/thekeep/state.cljs
@@ -1,21 +1,14 @@
 (ns thekeep.state
-  (:require [infinitelives.utils.vec2 :as vec2])
-  )
+  (:require [infinitelives.utils.vec2 :as vec2]))
 
 (def scale 2)
 
-(defonce state (atom
-                {:pos
-                 (vec2/vec2 (* 5 16) (* 5 16))
-
-                 :health 100
-                 :score 0
-
-                 :sword nil
-
-                 :enemy (vec2/zero)
-
-                 :enemies []
-
-                 :running? false
-                 }))
+(defonce state
+  (atom
+   {:pos (vec2/vec2 (* 5 16) (* 5 16))
+    :health 100
+    :score 0
+    :sword nil
+    :enemy (vec2/zero)
+    :enemies []
+    :running? false}))
