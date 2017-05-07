117320d53356b92547e94f90cb237755fce93ad1
Crispin Wellington <retrogradeorbit@gmail.com>
2017-05-07 21:33:30 +0800

    implemented support for async operations inside hash literal keys and values

--- src/thekeep/async.clj
+++ src/thekeep/async.clj
@@ -16,37 +16,49 @@
        (throw (js/Error "go-while exit")))))
 
 (defn thread-test-around-sync-points [test body]
-  (let [[head & tail] body]
-    (cond
-      ;; thread test condition around <!
-      (= head '<!)
-      (thread-test-around-sync-points test
-       (conj tail test 'thekeep.async/<!*))
+  (if (sequential? body)
+    (let [[head & tail] body]
+      (cond
+        ;; thread test condition around <!
+        (= head '<!)
+        (thread-test-around-sync-points
+         test
+         (conj tail test 'thekeep.async/<!*))
 
-      (= head '>!)
-      (thread-test-around-sync-points test
-       (conj tail test 'thekeep.async/>!*))
+        (= head '>!)
+        (thread-test-around-sync-points
+         test
+         (conj tail test 'thekeep.async/>!*))
 
-      :default
-      (for [form body]
-        (cond
-          ;; recurse through subforms of a vector
-          (vector? form) (into [] (thread-test-around-sync-points test form))
+        head
+        (for [form body]
+          (cond
 
-          ;; recurse through this full s-exp form
-          (sequential? form) (thread-test-around-sync-points test form)
+            ;; recurse through subforms of a vector
+            (vector? form) (into [] (thread-test-around-sync-points test form))
 
-          ;; todo: handle hashmap literals
+            ;; recurse through this full s-exp form
+            (sequential? form) (thread-test-around-sync-points test form)
 
-          ;; default is leave code unchanged
-          :default form)))))
+            ;; handle hashmap literals
+            (map? form)
+            (into {}
+                  (map
+                   (fn [[k v]]
+                     [(thread-test-around-sync-points test k)
+                      (thread-test-around-sync-points test v)])
+                   form))
+
+            ;; default is leave code unchanged
+            :default form))))
+    body))
 
 (defmacro go-while [test & body]
   `(go
-     ~(thread-test-around-sync-points test (cons 'do body))))
+     ~(thread-test-around-sync-points test body)))
 
 (defmacro continue-while [test & body]
   `(try
-     ~(thread-test-around-sync-points test (cons 'do body))
+     ~(thread-test-around-sync-points test body)
      (catch js/Error e
        (js/console.log "continue-while exit triggered by: " e))))
